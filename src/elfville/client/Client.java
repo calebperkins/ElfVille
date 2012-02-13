2725d12806c910e46cb5139e859643ff1a88958c
Caleb Perkins <frostmourne@gmail.com>
2012-02-13 13:03:12 -0500

    readme and Swing client

--- src/elfville/client/Client.java
+++ src/elfville/client/Client.java
@@ -1,17 +1,34 @@
 package elfville.client;
 
+import javax.swing.*;
+
 /**
  * Main entry point for client.
+ * 
  * @author caleb
- *
+ * 
  */
-public class Client {
+public class Client extends JFrame {
+
+	private static final long serialVersionUID = 2917659626432208915L;
+
+	public Client() {
+		setTitle("ElfVille");
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				Client ex = new Client();
+				ex.setVisible(true);
+			}
+		});
 	}
 
 }
