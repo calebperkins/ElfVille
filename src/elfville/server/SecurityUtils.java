1b3d772c7b2d191a3af11b384cbc1cacc933ecc9
Paul Yang <heranyang@hotmail.com>
2012-02-24 14:18:52 -0500

    one more file added

---
+++ src/elfville/server/SecurityUtils.java
@@ -0,0 +1,12 @@
+package elfville.server;
+
+public class SecurityUtils {
+
+	public static String encryptIntToString(int intNum) {
+		return new Integer(intNum).toString();
+	}
+	
+	public static int decryptStringToInt(String str) {
+		return Integer.parseInt(str);
+	}
+}
