3dff31a589dc581a9b01cc16b8e6586237e70271
Paul Yang <heranyang@hotmail.com>
2012-02-25 17:38:15 -0500

    added serializable models and testcases

---
+++ src/elfville/protocol/models/SerializableClan.java
@@ -0,0 +1,27 @@
+package elfville.protocol.models;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SerializableClan extends SerializableModel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public String clanName;
+	public String clanDescription;
+	public int numSocks;
+	public SerializableElf leader;
+	public List<SerializableElf> members;
+	public List<SerializablePost> posts;
+	public String clanID;
+	
+	public SerializableClan() {
+		super();
+		members = new ArrayList<SerializableElf>();
+		posts = new ArrayList<SerializablePost>();
+	}
+
+}
---
+++ src/testcases/AllTests.java
@@ -0,0 +1,11 @@
+package testcases;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ServerStart.class, AuthenticationTest.class })
+public class AllTests {
+
+}
