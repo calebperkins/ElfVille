acfb2b7e8fd78e4bc8aef6999d2269832d272bec
Caleb Perkins <frostmourne@gmail.com>
2012-02-27 13:15:32 -0500

    DB save/restore test

--- src/testcases/DatabaseSerializationTest.java
+++ src/testcases/DatabaseSerializationTest.java
@@ -2,17 +2,42 @@ package testcases;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 import elfville.server.Database;
+import elfville.server.model.*;
 
 public class DatabaseSerializationTest {
 
 	@Test
 	public void testDBrestore() {
-		Database db = new Database();
-		//db.elfDB.insert(elf)
-		fail("Not yet implemented");
+		Database.DB = new Database();
+		
+		Elf e = new Elf();
+		e.setDescription("Likes pork");
+		e.setElfName("Larry");
+		e.save();
+		
+		Clan c = new Clan("Pork Lovers", "Clan of Larry");
+		c.setLeader(e);
+		c.save();
+		
+		try {
+			Database.DB.writeToDisk("/tmp/elfville-test.db");
+		} catch (IOException e1) {
+			fail("Could not save database: " + e1.getMessage());
+		}
+		
+		try {
+			Database.DB = Database.load("/tmp/elfville-test.db");
+			assertEquals("Elfs persist", e.getElfName(), Database.DB.elfDB.findByID(e.getModelID()).getElfName());
+			assertEquals("Clans persist", c.getName(), Database.DB.clanDB.findByModelID(c.getModelID()).getName());
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			fail("Could not restore database: " + e1.getMessage());
+		}
 	}
 
 }
