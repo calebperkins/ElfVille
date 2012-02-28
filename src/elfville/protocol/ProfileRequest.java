7daebe5fd1bef8cd30aacc6fbc9b315f27452bf7
Brian Toth <bri.toth@gmail.com>
2012-02-28 17:01:31 -0500

    forgot to add new files

---
+++ src/elfville/protocol/ProfileRequest.java
@@ -0,0 +1,14 @@
+package elfville.protocol;
+
+import elfville.protocol.models.SerializableElf;
+
+public class ProfileRequest extends Request {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public SerializableElf elf;
+
+}
---
+++ src/elfville/protocol/ProfileResponse.java
@@ -0,0 +1,14 @@
+package elfville.protocol;
+
+import elfville.protocol.Response.Status;
+import elfville.protocol.models.SerializableElf;
+
+public class ProfileResponse extends Response {
+	private static final long serialVersionUID = 1L;
+
+	public ProfileResponse(Status s) {
+		this.status= s;
+	}
+	public SerializableElf elf;
+
+}
