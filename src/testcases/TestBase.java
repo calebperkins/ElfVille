232daec36b13df2ccbb1db7f3fb63e61868198ce
Paul Yang <heranyang@hotmail.com>
2012-02-27 19:17:34 -0500

    added back

---
+++ src/testcases/TestBase.java
@@ -0,0 +1,14 @@
+package testcases;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import elfville.client.SocketController;
+
+public class TestBase {
+	static List<SocketController> socketControllers = new ArrayList<SocketController>();
+}
