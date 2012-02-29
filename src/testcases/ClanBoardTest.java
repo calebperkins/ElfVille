bc7e51ec6ae53b8c1fa45265fc16c4c157db5ccf
Paul Yang <heranyang@hotmail.com>
2012-02-29 00:56:18 -0500

    clan board test done

--- src/testcases/ClanBoardTest.java
+++ src/testcases/ClanBoardTest.java
@@ -13,37 +13,95 @@ import elfville.protocol.models.SerializableClan;
 public class ClanBoardTest extends TestBase {
 	@Test
 	public void test1CreateClan() throws IOException {
-		for (int i = 0; i < 50; i++) {
-			String clanName= "clan-" + i;
-			String description= "description-" + i;
+		// Assert you can save clans successfully
+		for (int i = 0; i < clientNum; i++) {
+			String x = String.format("%2d", i); // needed to ensure lexographic order
+			String clanName= "clan-" + x;
+			String description= "description-" + x;
 			SerializableClan clan = new SerializableClan();
 			clan.clanName= clanName;
 			clan.clanDescription= description;
 			CreateClanRequest req= new CreateClanRequest(clan);
-			Response resp = socketControllers.get(0).send(req);
-			System.out.println(resp.status.toString());
+			Response resp = socketControllers.get(i % clientNum).send(req);
 			assertEquals(resp.status, Status.SUCCESS);
 		}
 	}
-
+	
 	@Test
-	public void test2getClans() throws IOException {
+	// All clients try to apply to all other clans. Check if this SUCCEED.
+	// Client try to apply to its own clan. Check if this FAIL.
+	public void test2ApplyClan() throws IOException {
+		
+	}
+	
+	@Test
+	// Clan owners accept half of the applications to check SUCCESS.
+	// CLan owners deny the other half of the applicants to check SUCCESS
+	public void test3ReplyApplicant() throws IOException {
+		
+	}
+	
+	@Test
+	// Clan owners and members create posts, check SUCCESS
+	// outsiders create posts on the clan board, check FAILURE
+	public void test4CreatePost() throws IOException {
+		
+		
+	}
+	
+	@Test
+	// Clan owners and member get Clan Board, check SUCCESS. The clan page should contain name, description, numSock, and private board. 
+	// Outsider get Clan Board, check SUCCESS. The clan page should contain everything except private board.
+	public void test5GetClanBoard() throws IOException {
+		
+	}
+	
+	@Test
+	// Clan owners and members remove posts, check SUCCESS
+	// outsiders remove posts on the clan board, check FAILURE
+	public void test6RemovePost() throws IOException {
+		
+		
+	}
+	
+	@Test
+	// Clan members leave Clan, check SUCCESS. Also check all posts on the clan posting board is gone.
+	// Retest all previous tests to make sure the left member is treated as an outsider.
+	// Check the member list in Clan does not contain this elf.
+	// Outsiders leave clan, check FAILURE
+	// Leader leaves clan, check FAILURE
+	public void test7LeaveClan() throws IOException {
+		
+	}
+	
+	@Test
+	// Leader disbands a clan, check SUCCESS. Check that Clan Directory does not contain it.
+	// Also recheck the former steps and expect FAILURE.
+	// Clan members try to disband a clan, check FAILURE.
+	// Outsiders try to disband a clan, check FAILURE.
+	public void test8DisbandClan() throws IOException {
+		
+	}
+	
+	@Test
+	public void test9getClans() throws IOException {
 		System.out.println("getClansTesting");
 		ClanListingRequest req = new ClanListingRequest();
 		ClanListingResponse resp = socketControllers.get(0).send(req);
-		System.out.println(resp.status.toString());
+		System.out.println(resp.status);
 		assertEquals(resp.status, Status.SUCCESS);
 		System.out.println(resp.clans.size());
 
-		for (int i = 0; i < 50; i++) {
+		for (int i = 0; i < clientNum; i++) {
+			String x = String.format("%2d", i); // needed to ensure lexographic order
 			SerializableClan clan = resp.clans.get(i);
 			System.out.println("what?");
 			System.out.println(clan.clanName);
 			System.out.println(clan.clanDescription);
 			System.out.println(clan.leader.elfName);
-			assertEquals("clan-" + i, clan.clanName);
-			assertEquals("description-" + i, clan.clanDescription);
-			assertEquals("user1", clan.leader.elfName);
+			assertEquals("clan-" + x, clan.clanName);
+			assertEquals("description-" + x, clan.clanDescription);
+			assertEquals("user" + (i % clientNum), clan.leader.elfName);
 		}
 
 	}
