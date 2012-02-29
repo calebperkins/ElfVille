ae7c4e2ecaeb2eca91a5ad1255082979b39d98aa
Caleb Perkins <frostmourne@gmail.com>
2012-02-28 23:30:12 -0500

    clan directory tests

--- src/testcases/ClanDirectoryTest.java
+++ src/testcases/ClanDirectoryTest.java
@@ -15,13 +15,14 @@ public class ClanDirectoryTest extends TestBase {
 	public void test1CreateClan() throws IOException {
 		// Assert you can save clans successfully
 		for (int i = 0; i < 50; i++) {
-			String clanName= "clan-" + i;
-			String description= "description-" + i;
+			String x = String.format("%2d", i); // needed to ensure lexographic order
+			String clanName= "clan-" + x;
+			String description= "description-" + x;
 			SerializableClan clan = new SerializableClan();
 			clan.clanName= clanName;
 			clan.clanDescription= description;
 			CreateClanRequest req= new CreateClanRequest(clan);
-			Response resp = socketControllers.get(0).send(req);
+			Response resp = socketControllers.get(i % clientNum).send(req);
 			System.out.println(resp.status);
 			assertEquals(resp.status, Status.SUCCESS);
 		}
@@ -37,14 +38,15 @@ public class ClanDirectoryTest extends TestBase {
 		System.out.println(resp.clans.size());
 
 		for (int i = 0; i < 50; i++) {
+			String x = String.format("%2d", i); // needed to ensure lexographic order
 			SerializableClan clan = resp.clans.get(i);
 			System.out.println("what?");
 			System.out.println(clan.clanName);
 			System.out.println(clan.clanDescription);
 			System.out.println(clan.leader.elfName);
-			assertEquals("clan-" + i, clan.clanName);
-			assertEquals("description-" + i, clan.clanDescription);
-			assertEquals("user1", clan.leader.elfName);
+			assertEquals("clan-" + x, clan.clanName);
+			assertEquals("description-" + x, clan.clanDescription);
+			assertEquals("user" + (i % clientNum), clan.leader.elfName);
 		}
 
 	}
