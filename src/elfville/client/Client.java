63aa4c194ca218caec103170be707aeb048cebad
Paul Yang <heranyang@hotmail.com>
2012-03-16 06:55:22 -0400

    added

--- src/elfville/client/Client.java
+++ src/elfville/client/Client.java
@@ -13,15 +13,22 @@ public class Client {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) throws Exception {
+		final String host;
+		final int port;
+		String publicKeyPath;
 		if (args.length != 3) {
-			System.err.println("Usage: host port /path/to/elfville.pub.der");
-			System.exit(-1);
+			System.out.println("Usage: host port /path/to/elfville.pub.der");
+			System.out.println("Not enough arguments. Default: localhost 8444 resources/elfville.pub.der");
+			host = "localhost";
+			port = 8444;
+			publicKeyPath = "resources/elfville.pub.der"; 
+		} else {
+			host = args[0];
+			port = Integer.parseInt(args[1]);
+			publicKeyPath = args[2];
 		}
-
-		final String host = args[0];
-		final int port = Integer.parseInt(args[1]);
 		
-		PublicKeyCipher.instance = new PublicKeyCipher(args[2]);
+		PublicKeyCipher.instance = new PublicKeyCipher(publicKeyPath);
 
 		EventQueue.invokeLater(new Runnable() {
 			@Override
