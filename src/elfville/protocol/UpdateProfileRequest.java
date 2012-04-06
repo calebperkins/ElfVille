3a08c8122d1bc98336d0521a29ca0402b90eb702
Brian Toth <bri.toth@gmail.com>
2012-04-06 12:08:35 -0400

    added protocol bit

---
+++ src/elfville/protocol/UpdateProfileRequest.java
@@ -0,0 +1,13 @@
+package elfville.protocol;
+
+public class UpdateProfileRequest extends Request {
+	private static final long serialVersionUID = 1L;
+
+	public String description;
+
+	public UpdateProfileRequest(String description) {
+		super();
+		this.description= description;
+	}
+
+}
