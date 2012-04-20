e9c631170eb9aa110ea749a98737eeb4e698ee7e
Caleb Perkins <frostmourne@gmail.com>
2012-04-20 02:10:13 -0400

    Database logging

--- src/elfville/server/Database.java
+++ src/elfville/server/Database.java
@@ -8,6 +8,8 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
@@ -22,7 +24,7 @@ import elfville.server.model.*;
  * Is a singleton.
  */
 public class Database {
-	private static Database instance = new Database();
+	private static Database instance;
 	private ObjectOutputStream stream = null;
 	public final ClanDB clanDB = new ClanDB();
 	public final PostDB postDB = new PostDB();
@@ -37,8 +39,43 @@ public class Database {
 	// getAndIncrementCountID() will increment this by 1.
 	private int countID = -1;
 
-	protected Database() {
-		// Solely to prevent outside instantiation.
+	private final Logger logger;
+
+	protected Database(String dbLocation) throws Exception {
+		logger = Logger.getLogger(dbLocation + " database");
+
+		try {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(
+					dbLocation));
+			SealedObject msg;
+			while (true) {
+				msg = (SealedObject) ois.readObject();
+				if (msg == null)
+					break;
+				Serializable m = SecurityUtils.decrypt(msg, dec);
+
+				if (m instanceof Model) {
+					Model x = (Model) m;
+					if (x.isDirty())
+						logger.warning(x + " is corrupted.");
+				}
+
+				if (m instanceof Clan) {
+					clanDB.add((Clan) m);
+				} else if (m instanceof Elf) {
+					elfDB.add((Elf) m);
+				} else if (m instanceof Post) {
+					postDB.add((Post) m);
+				} else if (m instanceof User) {
+					userDB.add((User) m);
+				} else if (m instanceof Deletion) {
+					((Deletion) m).deleteObject();
+				}
+			}
+		} catch (FileNotFoundException ex) {
+			logger.info(dbLocation + " not found. Creating...");
+		}
+		stream = new ObjectOutputStream(new FileOutputStream(dbLocation));
 	}
 
 	static public Database getInstance() {
@@ -51,9 +88,9 @@ public class Database {
 				Serializable msg = SecurityUtils.encrypt(obj, enc);
 				stream.writeUnshared(msg);
 			} catch (IOException e) {
-				System.err.println(obj + " could not be saved.");
+				logger.warning(obj + " could not be saved.");
 			} catch (IllegalBlockSizeException e) {
-				e.printStackTrace();
+				logger.log(Level.WARNING, "Bad clock size", e);
 			}
 		}
 		flush();
@@ -61,7 +98,7 @@ public class Database {
 
 	public void flush() {
 		if (stream != null) {
-			System.out.println("flush called!");
+			logger.info("flush called!");
 			try {
 				stream.flush();
 			} catch (IOException e) {
@@ -98,40 +135,7 @@ public class Database {
 		enc.init(Cipher.ENCRYPT_MODE, databaseSecret);
 		dec.init(Cipher.DECRYPT_MODE, databaseSecret);
 
-		try {
-			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(
-					dbLocation));
-			SealedObject msg;
-			// TODO: handle deleted objects
-			while (true) {
-				msg = (SealedObject) ois.readObject();
-				if (msg == null)
-					break;
-				Serializable m = SecurityUtils.decrypt(msg, dec);
-				
-				if (m instanceof Model) {
-					Model x = (Model) m;
-					if (x.isDirty())
-						System.err.println(x + " is corrupted.");
-				}
-
-				if (m instanceof Clan) {
-					instance.clanDB.add((Clan) m);
-				} else if (m instanceof Elf) {
-					instance.elfDB.add((Elf) m);
-				} else if (m instanceof Post) {
-					instance.postDB.add((Post) m);
-				} else if (m instanceof User) {
-					instance.userDB.add((User) m);
-				} else if (m instanceof Deletion) {
-					((Deletion) m).deleteObject();
-				}
-			}
-		} catch (FileNotFoundException ex) {
-			System.err.println(dbLocation + " not found. Creating...");
-		}
-		instance.stream = new ObjectOutputStream(new FileOutputStream(
-				dbLocation));
+		instance = new Database(dbLocation);
 	}
 
 	public synchronized int getAndIncrementCountID() {
